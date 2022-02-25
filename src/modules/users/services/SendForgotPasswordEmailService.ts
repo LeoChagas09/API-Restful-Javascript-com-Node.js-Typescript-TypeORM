063e277836aff55009a5e95f9fbc126848ce6655
Leonardo Chagas <leo_kikinhoo@hotmail.com>
2022-02-25 15:47:56 -0300

    Update SendForgotPasswordEmailService.ts
    Ajustes no Serviço de Envio de Email para Recuperação de Senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -4,6 +4,8 @@ import path from 'path';
 import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { UserTokensRepository } from '../typeorm/repositories/UserTokensRepository';
 import EtherealMail from '@config/mail/EtherealMail';
+import SESMail from '@config/mail/SESMail';
+import mailConfig from '@config/mail/mail';
 
 interface IRquest {
   email: string;
@@ -28,20 +30,22 @@ class SendForgotPasswordEmailService {
       'forgot_password.hbs',
     );
 
-    await EtherealMail.sendMail({
-      to: {
-        name: user.name,
-        email: user.email,
-      },
-      subject: '[API Vendas] Recuperação de Senha',
-      templateData: {
-        file: forgotPasswordTemplate,
-        variables: {
+    if (mailConfig.driver == 'ses') {
+      await SESMail.sendMail({
+        to: {
           name: user.name,
-          link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+          email: user.email,
         },
-      },
-    });
+        subject: '[API Vendas] Recuperação de Senha',
+        templateData: {
+          file: forgotPasswordTemplate,
+          variables: {
+            name: user.name,
+            link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`,
+          },
+        },
+      });
+    }
   }
 }
 
